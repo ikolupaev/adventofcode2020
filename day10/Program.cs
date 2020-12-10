66ef8287470fae4bc7c71fb38d8dc0a8d64fcc79
Igor Kolupaev <ikolupaev@gmail.com>
2020-12-10 07:21:09 -0500

    adds solution 1 of day 10

---
+++ day10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var volts = File
+                .ReadLines("input.txt")
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(volts);
+
+            var diffs = new int[4];
+            var last = 0;
+
+            foreach (var item in volts)
+            {
+                diffs[item - last]++;
+                last = item;
+            }
+
+            Console.WriteLine("answer #1: " + diffs[1] * (diffs[3]+1));
+        }
+    }
+}
