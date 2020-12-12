16e9cebe90b9bd26ad4ab17d8c0bbd52cd86b0cf
Igor Kolupaev <ikolupaev@gmail.com>
2020-12-12 08:35:18 -0500

    add solutions of day 12

---
+++ day12-1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var direction = 90;
+            var x = 0;
+            var y = 0;
+
+            foreach (var c in File.ReadLines("input.txt"))
+            {
+                var v = int.Parse(c[1..]);
+                var command = c[0];
+
+                if (command == 'F')
+                {
+                    switch (direction)
+                    {
+                        case 0: command = 'N'; break;
+                        case 90: command = 'E'; break;
+                        case 180: command = 'S'; break;
+                        case 270: command = 'W'; break;
+                        default: throw new Exception("unknown direction " + direction);
+                    }
+                }
+
+                switch (command)
+                {
+                    case 'N':
+                        y -= v;
+                        break;
+                    case 'S':
+                        y += v;
+                        break;
+                    case 'E':
+                        x += v;
+                        break;
+                    case 'W':
+                        x -= v;
+                        break;
+                    case 'R':
+                        direction = (direction + v) % 360;
+                        break;
+                    case 'L':
+                        direction -= v;
+                        if (direction < 0) direction += 360;
+                        direction %= 360;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Answer #1: " + Math.Abs(x + y));
+        }
+    }
+}
---
+++ day12-2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dx = 10;
+            var dy = 1;
+            var x = 0;
+            var y = 0;
+            foreach (var c in File.ReadLines("input.txt"))
+            {
+                var v = int.Parse(c[1..]);
+                var command = c[0];
+
+                switch (command)
+                {
+                    case 'N':
+                        dy += v;
+                        break;
+                    case 'S':
+                        dy -= v;
+                        break;
+                    case 'E':
+                        dx += v;
+                        break;
+                    case 'W':
+                        dx -= v;
+                        break;
+                    case 'R':
+                        {
+                            var rightRotations = v / 90;
+                            for (int i = 0; i < rightRotations; i++)
+                            {
+                                var t = -dx;
+                                dx = dy;
+                                dy = t;
+                            }
+                        }
+                        break;
+                    case 'L':
+                        {
+                            var rightRotations = 4 - v / 90;
+                            for (int i = 0; i < rightRotations; i++)
+                            {
+                                var t = -dx;
+                                dx = dy;
+                                dy = t;
+                            }
+                            break;
+                        }
+                    case 'F':
+                        x += dx * v;
+                        y -= dy * v;
+                        break;
+                    default:
+                        throw new Exception("unknown command " + c);
+                }
+            }
+
+            Console.WriteLine("Answer #1: " + Math.Abs(x + y));
+        }
+    }
+}
