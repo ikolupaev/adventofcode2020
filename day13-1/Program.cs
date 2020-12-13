b81ce24a695e1c2b703caa7f836cb3c41d1a6de5
Igor Kolupaev <ikolupaev@gmail.com>
2020-12-13 18:18:42 -0500

    added solutions for day 13

---
+++ day13-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day13_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var depart = int.Parse(input[0]);
+            var buses = input[1].Split(',').Where(x => x != "x").Select(int.Parse);
+
+            var bestWaitTime = int.MaxValue;
+            var bestBus = 0;
+            foreach (var bus in buses)
+            {
+                var waitTime = bus - depart % bus;
+                if(waitTime < bestWaitTime)
+                {
+                    bestWaitTime = waitTime;
+                    bestBus = bus;
+                }
+            }
+
+            Console.WriteLine(bestBus * bestWaitTime);
+        }
+    }
+}
---
+++ day13-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace day13_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var buses = new List<(long Id, long Minute)>();
+
+            long minute = 0;
+            foreach (var x in input[1].Split(','))
+            {
+                if (x != "x")
+                {
+                    buses.Add((long.Parse(x), minute));
+                }
+                minute++;
+            }
+
+            var timer = Stopwatch.StartNew();
+            var commonId = buses[0].Id;
+            var commonMinute = 100000000000000L / buses[0].Id * buses[0].Id + buses[0].Id;
+            foreach (var (busId, busMinute) in buses.Skip(1))
+            {
+                while ((commonMinute + busMinute) % busId != 0)
+                {
+                    commonMinute += commonId;
+                }
+
+                commonId *= busId;
+            }
+
+            Console.WriteLine("Answer 2: " + commonMinute);
+            Console.WriteLine("Elapsed: " + timer.Elapsed);
+        }
+    }
+}
