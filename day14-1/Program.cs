3cafbc8823e11e692c7dc033e3bbc198d21a3967
Igor Kolupaev <ikolupaev@gmail.com>
2020-12-14 17:24:30 -0500

    added solutions for day 14

---
+++ day14-1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day14_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memory = new List<ulong>();
+            var maskRegex = new Regex(@"mask = (.+)$");
+            var memRegex = new Regex(@"mem\[(?<address>\d+)\] = (?<value>\d+)$");
+
+            var mask1 = 0UL;
+            var mask0 = 0UL;
+
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                var mask = maskRegex.Match(line);
+                if (mask.Success)
+                {
+                    mask1 = 0;
+                    mask0 = 0;
+                    var maskStr = mask.Groups[1].Value.TrimStart('X').ToArray();
+
+                    foreach (var bit in maskStr)
+                    {
+                        mask0 <<= 1;
+                        mask1 <<= 1;
+
+                        switch (bit)
+                        {
+                            case 'X':
+                                mask0 |= 1;
+                                mask1 |= 0;
+                                break;
+                            case '1':
+                                mask0 |= 1;
+                                mask1 |= 1;
+                                break;
+                            case '0':
+                                mask0 |= 0;
+                                mask1 |= 0;
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    var mem = memRegex.Match(line);
+                    var address = int.Parse(mem.Groups["address"].Value);
+                    var value = ulong.Parse(mem.Groups["value"].Value);
+
+                    if (memory.Count < address + 1)
+                    {
+                        memory.AddRange(Enumerable.Repeat(0ul, address - memory.Count + 1));
+                    }
+                    
+                    value |= mask1;
+                    value &= mask0;
+                    
+                    memory[address] = value;
+                }
+            }
+
+            var answer = memory.Aggregate((a, b) => a += b);
+            Console.WriteLine("Answer #1: " + answer);
+        }
+    }
+}
---
+++ day14-2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day14_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memory = new Dictionary<ulong, ulong>();
+            var maskRegex = new Regex(@"mask = (.+)$");
+            var memRegex = new Regex(@"mem\[(?<address>\d+)\] = (?<value>\d+)$");
+
+            var mask1 = 0UL;
+            var floatingBits = new int[36];
+            var floatingBitsCounter = 0;
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                var mask = maskRegex.Match(line);
+                if (mask.Success)
+                {
+                    mask1 = 0;
+                    floatingBitsCounter = 0;
+                    var maskStr = mask.Groups[1].Value;
+
+                    for (var i = 0; i < maskStr.Length; i++)
+                    {
+                        mask1 <<= 1;
+
+                        if (maskStr[i] == 'X')
+                        {
+                            floatingBits[floatingBitsCounter++] = maskStr.Length - i - 1;
+                        }
+                        else
+                        {
+                            mask1 |= (uint)(maskStr[i] - '0');
+                        }
+                    }
+                }
+                else
+                {
+                    var mem = memRegex.Match(line);
+                    var address = ulong.Parse(mem.Groups["address"].Value);
+                    var value = ulong.Parse(mem.Groups["value"].Value);
+
+                    SetAddress(memory, value, address | mask1, floatingBits.AsSpan(0, floatingBitsCounter));
+                }
+            }
+
+            var answer = memory.Values.Aggregate((a, b) => a += b);
+            Console.WriteLine("Answer #2: " + answer);
+        }
+
+        private static void SetAddress(Dictionary<ulong, ulong> memory, ulong value, ulong address, Span<int> floatingBits)
+        {
+            if (floatingBits.Length == 0)
+            {
+                memory[address] = value;
+                return;
+            }
+
+            SetAddress(memory, value, address & ~(1UL << floatingBits[0]), floatingBits[1..]);
+            SetAddress(memory, value, address | 1UL << floatingBits[0], floatingBits[1..]);
+        }
+    }
+}
